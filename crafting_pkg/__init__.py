ad347e1d2005ca36cab9252bbc609bd63faefc18
CharGoon26 <chargonzales26@gmail.com>
2026-05-30 02:21:58 +0800

    Delete crafting_pkg directory

--- crafting_pkg/__init__.py
+++
@@ -1,2 +0,0 @@
-default_app_config = "crafting_pkg.apps.CraftingPkgConfig"
-
--- crafting_pkg/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CraftingPkgConfig(AppConfig):
-    name = "crafting_pkg"
-    dpy_package = "crafting_pkg.crafting_ext"
-
